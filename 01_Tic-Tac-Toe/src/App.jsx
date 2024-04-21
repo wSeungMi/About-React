ee9fba1386618bc38918a6aba0ca3b7aebb0dc7a
wSeungMi <seungmi.dev@gmail.com>
2024-04-21 20:45:54 +0900

    Feat: 틱택토 만들기 기본 기능 구현

---
+++ 01_Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,24 @@
+import Square from "./Square";
+import "./App.css";
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
---
+++ 01_Tic-Tac-Toe/src/Square.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
