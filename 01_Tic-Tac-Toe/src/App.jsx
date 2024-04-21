0b2729c51125b6c9e487f56b19ae4905bc6d9d14
wSeungMi <seungmi.dev@gmail.com>
2024-04-21 21:43:06 +0900

    Feat: X 표시

--- 01_Tic-Tac-Toe/src/App.jsx
+++ 01_Tic-Tac-Toe/src/App.jsx
@@ -1,23 +1,32 @@
 import Square from "./Square";
 import "./App.css";
+import { useState } from "react";
 
 export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+
+  function handleClick(idx) {
+    const nextSquares = [...square];
+    nextSquares[idx] = "X";
+    setSquare(nextSquares);
+  }
+  console.log(square);
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
